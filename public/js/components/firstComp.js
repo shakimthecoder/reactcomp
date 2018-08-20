46d4c5c5bbceef81372753d0b05bd0f110cd5b89
amenraprod <amenraprod@gmail.com>
2018-08-20 02:47:07 -0400

    moved image

--- public/js/components/firstComp.js
+++ public/js/components/firstComp.js
@@ -3,7 +3,7 @@ webpackJsonp([0],{
 /***/ 98:
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: SyntaxError: C:/Users/Nesu Bity/Desktop/react/react-redux/assets/js/firstComp/firstComp.js: Unexpected token (59:0)\n\n  57 | </header>)\n  58 | }\n> 59 | }\n     | ^\n  60 | \n  61 | \n  62 | \n");
+throw new Error("Module build failed: SyntaxError: C:/Users/Nesu Bity/Desktop/react/react-redux/assets/js/firstComp/firstComp.js: Unexpected token (16:0)\n\n  14 |   }\n  15 | \n> 16 | <Header /> \n     | ^\n  17 |  clickedBoy(){\n  18 |  this.setState({\n  19 |    health: this.state.health - 25\n");
 
 /***/ })
 
